63339ece145836f84dbe2e59b43eb05ec0b2e780
Guilherme <rodriguesgui48@gmail.com>
2025-11-03 15:43:16 -0300

    Mudei algumas descrições dos comandos de ajuda para facilitar o entendimento e deixar visualmente mais agradável.

--- cogs/gerais.py
+++ cogs/gerais.py
@@ -10,16 +10,16 @@ class Gerais(commands.Cog):
         embed = discord.Embed(
             title="Ajuda da Cyber",
             description="Lista de comandos disponíveis",
-            color=discord.Color.purple()
+            color=discord.Color.dark_red()
         )
 
         embed.add_field(
             name='Status',
             value=(
-                "**$criar_status** - cria o status do personagem\n"
-                "**$status** - busca o status atual do personagem\n"
-                "**$del_status** - deleta o status do personagem\n"
-                "**$update_status** - atualiza o valor de algum atributo do personagem\n"
+                "**criar_status** - *Cria o status do personagem*\n"
+                "**status** - *Busca o status atual do personagem*\n"
+                "**del_status** - *Deleta o status do personagem*\n"
+                "**update_status** - *Atualiza o valor de algum atributo do personagem*\n"
             ),
             inline = False
         )
@@ -27,7 +27,7 @@ class Gerais(commands.Cog):
         embed.add_field(
             name="Rolagem de Dados",
             value=(
-                "**$roll xdy** - gira dados de diferentes lados e faces, sendo 'x' a quantidade de dados, e 'y' a quantidade de faces. O 'd' é necessário para o funcionamento do comando"
+                "**roll** - *Rola os dados*"
             ),
             inline = False
         )
